e4d9469d182a848138870f5fbce63da28378635a
JeanC4rlo <jeancs.santos1@gmail.com>
2026-03-08 14:45:27 -0300

    Correção de eventos de mouse

--- games/tic-tac-toe/main.js
+++ games/tic-tac-toe/main.js
@@ -35,8 +35,6 @@ export default class TicTacToe extends Game {
                 }).appendTo($block);
 
                 $block.appendTo($line);
-
-                this.holding = false;
             }
 
             $line.appendTo($game);
@@ -57,19 +55,28 @@ export default class TicTacToe extends Game {
                 const $img = $block.find("img");
 
                 $block.on("mouseenter", () => {
-                    if (!this.holding) this.animateEnter($img);
+                    if (!$block.data("holding")) this.animateEnter($img);
                 });
                 $block.on("mouseleave", () => {
-                    if (!this.holding) this.animateLeave($img);
+                    if (!$block.data("holding")) this.animateLeave($img);
                 });
 
-                $block.on("mousedown touchstart", () => {
-                    this.holding = true;
+                $block.on("pointerdown", () => {
+                    $block.data("holding", true);
                     this.animateDown($img);
+
+                    $block.off("pointerleave");
+
+                    $block.on("pointerleave", () => {
+                        $block.data("holding", false);
+                        this.animateClear($img);
+                        $block.off("pointerleave");
+                    });
                 });
 
-                $block.on("mouseup touchend", () => {
-                    this.holding = false;
+                $block.on("pointerup", () => {
+                    $block.data("holding", false);
+                    $block.off("pointerleave");
                     this.animateUp($img);
                     this.round($block);
                 });
@@ -78,17 +85,33 @@ export default class TicTacToe extends Game {
     }
 
     animate($img, frames, i = 0) {
+        const prevTimer = $img.data("animTimer");
+        if (prevTimer) clearTimeout(prevTimer);
+
         if (i >= frames.length) return;
 
         $img.attr("src", frames[i].src);
 
-        setTimeout(() => {
+        const timer = setTimeout(() => {
             this.animate($img, frames, i + 1);
         }, frames[i].time);
+
+        $img.data("animTimer", timer);
+    }
+
+    animateClear($img) {
+        if ($img.data("animTimer")) {
+            clearTimeout($img.data("animTimer"));
+        }
+
+        const base = new URL('.', import.meta.url);
+        $img.attr("src", `${base}/assets/blank.png`);
     }
 
     animateEnter($img) {
         const base = new URL('.', import.meta.url);
+        this.animateClear($img);
+        clearTimeout(this.actualAnim);
 
         if (this.turno == TicTacToe.X) {
             const framesX = [
@@ -110,6 +133,7 @@ export default class TicTacToe extends Game {
 
     animateLeave($img) {
         const base = new URL('.', import.meta.url);
+        this.animateClear($img);
 
         if (this.turno == TicTacToe.X) {
             const framesX = [
@@ -133,6 +157,7 @@ export default class TicTacToe extends Game {
 
     animateDown($img) {
         const base = new URL('.', import.meta.url);
+        this.animateClear($img);
 
         if (this.turno == TicTacToe.X) {
             const framesX = [
@@ -152,6 +177,7 @@ export default class TicTacToe extends Game {
 
     animateUp($img) {
         const base = new URL('.', import.meta.url);
+        this.animateClear($img);
 
         if (this.turno == TicTacToe.X) {
             const framesX = [
@@ -169,8 +195,9 @@ export default class TicTacToe extends Game {
         }
     }
 
-    animateClear($img) {
+    animateReset($img) {
         const base = new URL('.', import.meta.url);
+        this.animateClear($img);
 
         const frames = [
             { src: `${base}/assets/pressed.png`, time: 300 },
@@ -181,12 +208,12 @@ export default class TicTacToe extends Game {
     }
 
     round($block) {
-        const row = $block.attr("data-row");
-        const col = $block.attr("data-col");
+        const row = Number($block.attr("data-row"));
+        const col = Number($block.attr("data-col"));
 
         this.board[row][col] = this.turno;
         $block.addClass("active");
-        $block.off();
+        $block.off("mouseenter mouseleave mousedown mouseup touchstart touchend");
 
         this.check();
 
@@ -210,30 +237,42 @@ export default class TicTacToe extends Game {
             [[0, 2], [1, 1], [2, 0]]
         ]
 
-        for(const line of lines) {
+        for (const line of lines) {
             let b0 = this.board[line[0][0]][line[0][1]];
             let b1 = this.board[line[1][0]][line[1][1]];
             let b2 = this.board[line[2][0]][line[2][1]];
 
-            if(b0 && b0 === b1 && b1 === b2) {
+            if (b0 && b0 === b1 && b1 === b2) {
                 console.log("Vencedor: " + b0);
-                this.clear();
+                this.animateClear();
+                return;
             }
         }
+
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (!this.board[i][j])
+                    return;
+            }
+        }
+
+        console.log("Velha!");
+        this.reset();
     }
 
-    clear() {
+    reset() {
         const $lines = $(".game").children();
 
         $lines.each((i, line) => {
             const $blocks = $(line).children();
             $blocks.each((j, block) => {
                 $(block).removeClass("active");
+                $(block).off("mouseenter mouseleave mousedown mouseup touchstart touchend");
                 const $img = $(block).find("img");
 
                 const index = (i * 3) + j;
                 setTimeout(() => {
-                    this.animateClear($img);
+                    this.animateReset($img);
                 }, index * 100);
             });
         });
